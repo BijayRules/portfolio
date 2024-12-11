a64c26d1143e3dfb5920d395aafb5c5c86e45e7a
bijayshrestha14 <bijayshrestha01238@gmail.com>
2024-12-11 23:45:37 +0545

    pushed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -77,6 +77,144 @@
         </div>
       </section>
 
+      <!-- Section - Resume -->
+      <section class="lui-section lui-gradient-bottom" id="resume-section">
+        <!-- Heading -->
+        <div class="lui-heading">
+          <div class="container max-w-[1360px] mx-auto  ">
+
+            <div class="m-titles align-center">
+              <h2 class="m-title" data-splitting="words">
+                <span> Resume </span>
+              </h2>
+              <div class="m-subtitle" data-splitting="words">
+                <span> my <b>Story</b> </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- History -->
+        <div class="v-line v-line-left">
+          <div class="container max-w-[1360px] mx-auto px-10">
+
+            <div class="grid md:grid-cols-2 gap-10">
+              <div class="edu__section">
+                <h5 class="history-title">
+                  <span> Education </span>
+                </h5>
+                <div class="history-items">
+                  <div class="history-item lui-collapse-item opened">
+                    <h6 class="name lui-collapse-btn active">
+                      <span> Halftone Designs </span>
+                    </h6>
+                    <div class="history-content">
+                      <div class="subname">
+                        <span>Web Designing Courses </span>
+                      </div>
+                      <div class="date lui-subtitle">
+                        <span> 2018 - 2019 </span>
+                      </div>
+                      <div class="text">
+                        <div>
+                          <p>
+                            Successfully completed the Web Designing course from Halftone Designs Pvt. Ltd.
+                            and able to convert my UI Design into pixel perfect Responsive HTML5/CSS3.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="exp__section">
+                <h5 class="history-title">
+                  <span> Experience </span>
+                </h5>
+                <div class="history-items">
+                  <div class="history-item lui-collapse-item opened">
+                    <h6 class="name lui-collapse-btn active">
+                      <span>Frontend Developer </span>
+                    </h6>
+                    <div class="history-content">
+                      <div class="subname">
+                        <span> Genesis Web Technology</span>
+                      </div>
+                      <div class="date lui-subtitle">
+                        <span> 2020 - <b>Present</b> </span>
+                      </div>
+                      <div class="text">
+                        <div>
+                          <p>
+                            I am currently working as a Frontend Developer at a company specializing in WordPress
+                            development. My role involves creating and maintaining user interfaces for WordPress-based
+                            projects using HTML, CSS, and JavaScript. I collaborate with a diverse team to ensure
+                            high-quality, visually appealing, and user-friendly websites and web applications.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="history-item lui-collapse-item opened">
+                    <h6 class="name lui-collapse-btn active">
+                      <span> Web Designer, Trainee </span>
+                    </h6>
+                    <div class="history-content">
+                      <div class="subname">
+                        <span> Halftone Designs </span>
+                      </div>
+                      <div class="date lui-subtitle">
+                        <span> 2019 - 2023 </span>
+                      </div>
+                      <div class="text">
+                        <div>
+                          <p>
+                            During my tenure, I had the opportunity to learn and teach web designs to many other
+                            teaching the latest frameworks, skills.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- <div class="lui-bgtitle">
+              <span> History </span>
+            </div> -->
+          </div>
+        </div>
+      </section>
+
+      <!-- Section - Skills -->
+      <section class="lui-section lui-gradient-center" id="skills-section">
+        <!-- Heading -->
+        <div class="lui-heading">
+          <div class="container">
+            <div class="m-titles align-center">
+              <h2 class="m-title">
+                <span> Professional Skills </span>
+              </h2>
+              <div class="m-subtitle">
+                <span> my <b>Talent</b> </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Skills -->
+        <div class="v-line v-line-left">
+          <div class="container">
+
+
+            <!-- <div class="lui-bgtitle">
+                <span> Skills </span> -->
+          </div>
+        </div>
+      </section>
+
+
     </div>
   </main>
 </template>
